65abbb1580f86460c1d4c474e33548f160ceb399
Bryan Tsanga <bryan-t@hotmail.fr>
2024-04-18 15:27:03 +0200

    started the models

---
+++ models/Audio.model.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require("mongoose");
+
+const audioSchema = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Audio = model("Audio", audioSchema);
+
+module.exports = Audio;
